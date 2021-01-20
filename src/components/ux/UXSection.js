4c1596c7f3a25144a7347b303d46d2476e76df20
Zenb0t <46074517+Zenb0t@users.noreply.github.com>
2021-01-20 09:27:51 -0800

    iframe update

--- src/components/ux/UXSection.js
+++ src/components/ux/UXSection.js
@@ -5,7 +5,7 @@ const UXSection = (props) => {
         <iframe
             width="800"
             height="450"
-            src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2F4YFuVDuIjAd3emltdMwRkM%2FESSS-Prototype%3Fnode-id%3D0%253A1"
+            src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2F4YFuVDuIjAd3emltdMwRkM%2FESSS-Prototype%3Fscaling%3Dmin-zoom%26node-id%3D320%253A2103"
             allowFullScreen>
 
         </iframe>
